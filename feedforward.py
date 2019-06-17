f8b59a844d0d9854844e3e75c3121ffd1ba6e3d7
turtlebasket <turtlebasket@gmail.com>
2019-06-17 10:40:29 -0700

    got PCAP loading to work

--- feedforward.py
+++ feedforward.py
@@ -1,50 +1,51 @@
-# CLEVERWALL - MAIN
-# ** Currently unusable, and still a MASSIVE WIP.
+"""
+CLEVERWALL - MAIN
+** Currently unusable, and still a MASSIVE WIP.
+"""
 
 import torch
 from torch import nn
-import os
-from scapy.utils import rdpcap
+from scapy.all import rdpcap, hexdump
 
-# Hyper-parameters 
-input_size = 784
-hidden_size = 500
-num_classes = 10
-num_epochs = 5
-batch_size = 100
-learning_rate = 0.001
+# Hyper-parameters
+INPUT_SIZE = 784
+HIDDEN_SIZE = 500
+NUM_CLASSES = 10
+NUM_EPOCHS = 5
+BATCH_SIZE = 100
+LEARNING_RATE = 0.001
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # ok so it looks like torch.device() got thanos snapped by the pytorch devs
 # ** RESOLVE LATER
 
-# load dataset from wireshark PCAP
+"""
+Extend torch's default Dataset class
+load dataset from wireshark PCAP
+"""
 class TrafficDataset(torch.utils.data.Dataset):
+
     def __init__(self, filename):
-        self.capture_reader= rdpcap(filename, 1000)
-        self.packets = []
-        for (pkt_data, pkt_metadata) in self.capture_reader:
-            # Append array to array. Should make data/metadata easier to track
-            self.packets.append(tuple((pkt_data, pkt_metadata))) # pass in tuple
+        self.capture_reader = rdpcap(filename, 1000)
 
     # return length of packet list
-    def __len__(self): 
-        return len(self.packets)# FIX LATER.
-    
+    def __len__(self):
+        return len(self.capture_reader)# FIX LATER.
+
     # return item to whoever's reading from dataset
     def __getitem__(self, index):
-        return self.packets[index]
-        
-train_dataset = TrafficDataset('./data/train.pcap')
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset)
+        return hexdump(self.capture_reader)
+
+TRAIN_DATASET = TrafficDataset('./data/train.pcap')
+TRAIN_LOADER = torch.utils.data.DataLoader(dataset=TRAIN_DATASET)
 
 # basic nn class setup
 class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+    def __init__(self, INPUT_SIZE, HIDDEN_SIZE, NUM_CLASSES):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size) 
+        self.fc1 = nn.Linear(INPUT_SIZE, HIDDEN_SIZE)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)  
+        self.fc2 = nn.Linear(HIDDEN_SIZE, NUM_CLASSES)
 
     def forward(self, x):
         out = self.fc1(x)
